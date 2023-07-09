0c4d243851180a0216bc1d12776c448a2fe54c0e
yakubsiyam <itsyakubsiyam@gmail.com>
2023-07-10 00:41:59 +0600

    Replace live link

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://technet-server.onrender.com',
+  }),
   tagTypes: ['comments'],
   endpoints: () => ({}),
 });
